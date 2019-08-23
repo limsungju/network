44d390eec8efa2df9b8214e770e46a5344053696
limsungju <fullmoon4860@gmail.com>
2019-08-23 09:24:41 +0900

    Echo Server ver1 완성

--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,8 +1,10 @@
 package echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -23,41 +25,37 @@ public class EchoServer {
 			InetAddress inetAddress = InetAddress.getLocalHost(); // Inet Address 얻어오기.
 			InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, PORT);
 			serverSocket.bind(inetSocketAddress);
-			System.out.println("[EchoServer] binding " + inetAddress.getHostAddress() + ":" + PORT);
+			log("binding " + inetAddress.getHostAddress() + ":" + PORT);
 			
 			// 3. accept: 클라이언트로 부터 연결요청(Connect)을 기다린다.
-			Socket socket = serverSocket.accept();
+			Socket socket = serverSocket.accept(); // Blocking
 			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress(); // 클라이언트의 Inet Address 얻어오기
-			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress(); // 클라이언트의 IP Address 얻어오기
-			int remoteHostPort = inetRemoteSocketAddress.getPort(); // 클라이언트의 PORT 얻어오기
 			
-			System.out.println("[EchoServer] connected from client [" + remoteHostAddress + ":" + remoteHostPort + "]");
+			// 클라이언트의 IP Address 얻어오기, 클라이언트의 PORT 얻어오기
+			log("connected from client [" + inetRemoteSocketAddress.getAddress().getHostAddress() + ":" + inetRemoteSocketAddress.getPort() + "]");
 			
 			try {
-				// 4. IOStream 받아오기
-				InputStream is = socket.getInputStream();
-				OutputStream os = socket.getOutputStream();
+				// 4. I/O Stream 생성하기
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true); // true -> flush옵션 버퍼가 채워질때마다 출력하기.
 				
 				while(true) {
-					// 5. 데이터 읽기
-					byte[] buffer = new byte[256];
-					int readByteCount = is.read(buffer); // Blocking
-					if(readByteCount == -1) {
+					// 5. 데이터 읽기(수신)
+					String data = br.readLine();
+					if(data == null) {
 						// 정상종료: remote socket이 close() 메소드를 통해서 정상적으로 소켓을 닫은 경우
-						System.out.println("[EchoServer] closed by client");
+						log("closed by client");
 						break;
 					}
+					log("received: " + data);
 					
-					String data = new String(buffer, 0, readByteCount, "UTF-8"); // 읽은 수만큼 decoding 작업
-					System.out.println("[EchoServer] received: " + data);
-					
-					// 6. 데이터 쓰기
-					os.write(data.getBytes("UTF-8")); // 읽은 수만큼 encoding 작업
+					// 6. 데이터 쓰기(송신)
+					pw.println(data);
 				}
 				
 				
 			} catch(SocketException e) {
-				System.out.println("[EchoServer] abnormal closed by client");
+				log("abnormal closed by client");
 			} catch(IOException e) {
 				e.printStackTrace();
 			} finally {
@@ -80,4 +78,9 @@ public class EchoServer {
 			}
 		}
 	}
+	
+	public static void log(String log) {
+		System.out.println("[Echo Server] " + log);
+	}
+	
 }
