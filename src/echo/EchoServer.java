bc32dc0f460e35e40be81e2ec76c294a4a925887
limsungju <fullmoon4860@gmail.com>
2019-08-23 11:34:51 +0900

    Thread 이용방법

--- src/echo/EchoServer.java
+++ src/echo/EchoServer.java
@@ -1,15 +1,10 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
 	private static final int PORT = 8000;
@@ -28,41 +23,11 @@ public class EchoServer {
 			log("binding " + inetAddress.getHostAddress() + ":" + PORT);
 			
 			// 3. accept: 클라이언트로 부터 연결요청(Connect)을 기다린다.
-			Socket socket = serverSocket.accept(); // Blocking
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress(); // 클라이언트의 Inet Address 얻어오기
-			
-			// 클라이언트의 IP Address 얻어오기, 클라이언트의 PORT 얻어오기
-			log("connected from client [" + inetRemoteSocketAddress.getAddress().getHostAddress() + ":" + inetRemoteSocketAddress.getPort() + "]");
-			
-			try {
-				// 4. I/O Stream 생성하기
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true); // true -> flush옵션 버퍼가 채워질때마다 출력하기.
-				
-				while(true) {
-					// 5. 데이터 읽기(수신)
-					String data = br.readLine();
-					if(data == null) {
-						// 정상종료: remote socket이 close() 메소드를 통해서 정상적으로 소켓을 닫은 경우
-						log("closed by client");
-						break;
-					}
-					log("received: " + data);
-					
-					// 6. 데이터 쓰기(송신)
-					pw.println(data);
-				}
-				
-				
-			} catch(SocketException e) {
-				log("abnormal closed by client");
-			} catch(IOException e) {
-				e.printStackTrace();
-			} finally {
-				// 7. Socket 자원정리
-				if(socket != null && socket.isClosed() == false) {
-					socket.close();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept(); // Blocking
+//				Thread thread = new EchoServerReceiveThread(socket);
+//				thread.start();
+				new EchoServerReceiveThread(socket).start();
 			}
 			
 		} catch (IOException e) {
@@ -79,8 +44,8 @@ public class EchoServer {
 		}
 	}
 	
-	private static void log(String log) {
-		System.out.println("[Echo Server] " + log);
+	public static void log(String log) {
+		System.out.println("[Echo Server#" + Thread.currentThread().getId() + "]" + log);
 	}
 	
 }
---
+++ src/thread/MultiThreadEx03.java
@@ -0,0 +1,14 @@
+package thread;
+
+public class MultiThreadEx03 {
+
+	public static void main(String[] args) {
+		Thread thread1 = new DigitThread();
+		Thread thread2 = new AlphabetThread();
+		Thread thread3 = new Thread(new UppercaseAlphabetRunnableImpl());
+		
+		thread1.start();
+		thread2.start();
+		thread3.start();
+	}
+}
