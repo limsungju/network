ed17b470597a3cdaf92cf49c87e32ec3568ef58d
limsungju <fullmoon4860@gmail.com>
2019-08-26 14:35:27 +0900

    UDP Time Server, UDP Time Client init commit

---
+++ src/udp/UDPTimeClient.java
@@ -0,0 +1,71 @@
+package udp;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class UDPTimeClient {
+	private static final String SERVER_IP = "192.168.1.3";
+
+	public static void main(String[] args) {
+		Scanner scanner = null;
+		DatagramSocket socket = null;
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a");
+		String dateData = format.format(new Date());
+		
+		try {
+			// 1. 키보드 연결
+			scanner = new Scanner(System.in);
+
+			// 2. 소켓 생성
+			socket = new DatagramSocket(); // 클라이언트 UDP 소켓은 PORT가 필요없다.
+
+			while (true) {
+				// 3. 사용자 입력을 받음
+				System.out.print(">>");
+				String message = scanner.nextLine();
+
+				if ("quit".equals(message)) {
+					break;
+				}
+				
+
+				// 4. 메세지 전송
+				byte[] sendData = message.getBytes("UTF-8");
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
+						new InetSocketAddress(SERVER_IP, UDPTimeServer.PORT));
+				socket.send(sendPacket);
+
+				// 5. 메세지 수신
+				DatagramPacket receivePacket = new DatagramPacket(new byte[UDPTimeServer.BUFFER_SIZE],
+						UDPTimeServer.BUFFER_SIZE);
+				socket.receive(receivePacket); // Blocking
+
+				// 6. data 처리(확인)
+				byte[] data = receivePacket.getData();
+				int length = receivePacket.getLength();
+				message = new String(data, 0, length, "UTF-8");
+				if("".equals(message)) {
+					System.out.println("<<" + dateData);
+				} else {
+					System.out.println("<<" + message);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+			if (socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
+
+	}
+}
